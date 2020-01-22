0343403150d7c60c72d1a981e5706197a340187c
jesuschule <44979437+jesuschule@users.noreply.github.com>
2020-01-22 04:57:52 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,60 @@
+
+
+
+//-- Plugin implementation
+(function($) {
+  $.fn.countTo = function(options) {
+    return this.each(function() {
+      //-- Arrange
+      var FRAME_RATE = 60; // Predefine default frame rate to be 60fps
+      var $el = $(this);
+      var countFrom = parseInt($el.attr('data-count-from'));
+      var countTo = parseInt($el.attr('data-count-to'));
+      var countSpeed = $el.attr('data-count-speed'); // Number increment per second
+
+      //-- Action
+      var rafId;
+      var increment;
+      var currentCount = countFrom;
+      var countAction = function() {              // Self looping local function via requestAnimationFrame
+        if(currentCount < countTo) {              // Perform number incremeant
+          $el.text(Math.floor(currentCount));     // Update HTML display
+          increment = countSpeed / FRAME_RATE;    // Calculate increment step
+          currentCount += increment;              // Increment counter
+          rafId = requestAnimationFrame(countAction);
+        } else {                                  // Terminate animation once it reaches the target count number
+          $el.text(countTo);                      // Set to the final value before everything stops
+          //cancelAnimationFrame(rafId);
+        }
+      };
+      rafId = requestAnimationFrame(countAction); // Initiates the looping function
+    });
+  };
+}(jQuery));
+
+//-- Executing
+$('.number-counter').countTo();
+new WOW().init();
+
+smoothScroll.init({
+    speed: 1000, // Integer. How fast to complete the scroll in milliseconds
+    offset: 100, // Integer. How far to offset the scrolling anchor location in pixels
+
+});
+
+$('.navbar-collapse>ul>li>a').click(function() {
+     $('.navbar-collapse').collapse('hide');});
+
+$(function () {
+    $(window).scroll(function () {
+        var scrolltop = $(this).scrollTop();
+        if (scrolltop >= 50) {
+            $(".ir-arriba").fadeIn();
+        } else {
+            $(".ir-arriba").fadeOut();
+        }
+    });
+
+});
+
+
